30d9043efe3fc0c973ac10e0e5bf668d6cde926a
illyrius666 <fitimq@live.nl>
2025-06-20 09:43:54 +0200

    Rename main.py to bot.py and implement basic Discord bot functionality; update dependencies and .gitignore
    Signed-off-by: illyrius666 <fitimq@live.nl>

---
+++ src/bot.py
@@ -0,0 +1,28 @@
+#   Copyright (c) 2025. Xodium.
+#   All rights reserved.
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello! I am your bot.')
+
+load_dotenv()
+
+token = os.getenv('DISCORD_BOT_TOKEN')
+if not token:
+    raise ValueError("DISCORD_BOT_TOKEN is not set in the .env file")
+
+bot.run(token)
