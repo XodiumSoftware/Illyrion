1ceeb9d1188786185e8d93603fdf0f55d7b176c2
illyrius666 <fitimq@live.nl>
2025-06-20 11:37:07 +0200

    Refactor bot implementation to use discord.Bot; update dependencies to py-cord
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/bot.py
+++ src/bot.py
@@ -1,50 +1,16 @@
 #   Copyright (c) 2025. Xodium.
 #   All rights reserved.
 
-import os
-from functools import wraps
-
 import discord
-from discord import Intents, Interaction, Object
-from discord.app_commands import CommandTree
-from dotenv import load_dotenv
 
-def log_cmd(func):
-    @wraps(func)
-    async def wrapper(interaction: Interaction, *args, **kwargs):
-        cmd_name = func.__name__
-        print(f'Command "{cmd_name}" used by @{interaction.user} ({interaction.user.id}) in #{interaction.channel} ({interaction.channel.id})')
-        response = await func(interaction, *args, **kwargs)
-        print(f'Responded with "{response}" to @{interaction.user} ({interaction.user.id}) in #{interaction.channel} ({interaction.channel.id})')
-        return response
-    return wrapper
+bot = discord.Bot()
 
-class Bot(discord.Client):
-    def __init__(self):
-        intents = Intents.default()
-        intents.message_content = True
-        super().__init__(intents=intents)
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
 
-        load_dotenv()
+@bot.command(description="Sends the bot's latency.")
+async def ping(ctx):
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
 
-        self.token = os.getenv('DISCORD_BOT_TOKEN')
-        if not self.token: raise ValueError("DISCORD_BOT_TOKEN is not set in the .env file")
-
-        self.tree = CommandTree(self)
-        self.guild = Object(id=691029695894126623)
-
-    async def setup_hook(self):
-        @log_cmd
-        @self.tree.command(name="test", description="Responds with pong")
-        async def ping(interaction: Interaction):
-            await interaction.channel.send("pong")
-
-        await self.tree.sync(guild=self.guild)
-
-    async def on_ready(self): print(f'Logged on as {self.user} ({self.user.id})')
-
-    def run(self, **kwargs):
-        super().run(self.token)
-
-if __name__ == '__main__':
-    Bot().run()
+bot.run("TOKEN")
