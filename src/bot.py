bcd461c54de0324cd9d1fcbe19dc991fce3a7cec
illyrius666 <fitimq@live.nl>
2025-06-20 10:46:29 +0200

    Enhance bot functionality by adding command logging and updating dependencies; refactor bot class structure
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/bot.py
+++ src/bot.py
@@ -2,36 +2,47 @@
 #   All rights reserved.
 
 import os
+from functools import wraps
+
 import discord
-from discord.ext import commands
+from discord import Intents
 from dotenv import load_dotenv
 
-class Bot:
+def log_cmd(cmd):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(self, message, *args, **kwargs):
+            if message.author == self.user:
+                return await func(self, message, *args, **kwargs)
+            print(f'Command "{cmd}" used by @{message.author} ({message.author.id}) in #{message.channel} ({message.channel.id})')
+            response = await func(self, message, *args, **kwargs)
+            print(f'Responded with "{response}" to @{message.author} ({message.author.id}) in #{message.channel} ({message.channel.id})')
+            return response
+        return wrapper
+    return decorator
+
+class Bot(discord.Client):
     def __init__(self):
-        load_dotenv()
-        self.token = os.getenv('DISCORD_BOT_TOKEN')
-        if not self.token:
-            raise ValueError("DISCORD_BOT_TOKEN is not set in the .env file")
-
-        intents = discord.Intents.default()
+        intents = Intents.default()
         intents.message_content = True
+        super().__init__(intents=intents)
 
-        self.bot = commands.Bot(command_prefix='!', intents=intents)
-        self._register_handlers()
+        load_dotenv()
 
-    def _register_handlers(self):
-        self.bot.event(self.on_ready)
-        self.bot.command()(self.hello)
+        self.token = os.getenv('DISCORD_BOT_TOKEN')
+        if not self.token: raise ValueError("DISCORD_BOT_TOKEN is not set in the .env file")
 
     async def on_ready(self):
-        print(f'Logged in as {self.bot.user}')
+        print(f'Logged on as {self.user}')
 
-    @staticmethod
-    async def hello(ctx):
-        await ctx.send('Hello! I am your bot.')
+    @log_cmd('ping')
+    async def on_message(self, message):
+        if message.author == self.user: return
+        if message.content == 'ping':
+            await message.channel.send('pong')
 
-    def run(self):
-        self.bot.run(self.token)
+    def run(self, **kwargs):
+        super().run(self.token)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     Bot().run()
