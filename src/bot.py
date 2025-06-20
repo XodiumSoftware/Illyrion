2d7dda3301bcfa7804ede250fdb704a9ccceb9b9
illyrius666 <fitimq@live.nl>
2025-06-20 11:46:25 +0200

    Add environment variable loading and improve logging; update dependencies
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,25 @@
 #   Copyright (c) 2025. Xodium.
 #   All rights reserved.
 
+import os
+
 import discord
+from dotenv import load_dotenv
 
 bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} is ready and online!")
+    print(f"[LOG] {bot.user} (ID={bot.user.id}) is ready and online!")
 
 @bot.command(description="Sends the bot's latency.")
 async def ping(ctx):
     await ctx.respond(f"Pong! Latency is {bot.latency}")
+    print(f"[LOG] @{ctx.author} (ID={ctx.author.id}) used /ping in #{ctx.channel} (ID={ctx.channel.id}).")
 
-bot.run("TOKEN")
+load_dotenv()
+token = os.environ.get("TOKEN")
+if not token:
+    raise ValueError("No TOKEN found in environment variables. Please set the TOKEN variable.")
+
+bot.run(token)
