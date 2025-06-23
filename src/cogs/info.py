308b277b53f4961f3205c49b6e856341103c1f61
illyrius666 <fitimq@live.nl>
2025-06-23 11:27:55 +0200

    Rename IP cog to Info and update command to display server information
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/cogs/info.py
+++ src/cogs/info.py
@@ -5,24 +5,24 @@ import discord
 from discord.ext import commands
 
 
-class IP(commands.Cog):
+class Info(commands.Cog):
     """
-    A cog for handling IP-related commands.
+    A cog for handling Info-related commands.
     """
 
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.slash_command(description="Returns the server IP address.")
-    async def ip(self, ctx):
+    @discord.slash_command(description="Displays the server info.")
+    async def info(self, ctx):
         await ctx.send_response(
             embed=discord.Embed(
-                title="ℹ️ Server IP:",
-                description="`illyria.xodium.org`",
+                title="ℹ️ Server Info:",
+                description=("IP: `illyria.xodium.org`\nVersion: `1.21.6`"),
                 color=discord.Color.blue(),
             )
         )
 
 
 def setup(bot):
-    bot.add_cog(IP(bot))
+    bot.add_cog(Info(bot))
